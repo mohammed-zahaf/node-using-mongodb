3a4efd50b45fa0b0156e25ed601988a5f661e2d3
mzahaf <mohammed.zahaf@gmail.com>
2021-03-07 12:15:53 +0100

    feat(module2): add mongoose connexion
    Signed-off-by: mzahaf <mohammed.zahaf@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import { routes } from './src/routes/routes';
 
 const app = express();
 const PORT = 4000;
+const DB_NAME = 'PRODUCT_DB';
+
+// Setting modules
+mongoose.Promise = global.Promise;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send(`Store server running on port ${PORT}`));
+// Mongoose connection
+mongoose.connection.on('open', () => console.log('Connected to mongo server.'));
+mongoose.connection.on('error', (err) => {
+  console.log('Could not connect to mongo server!');
+  return console.log(err);
+});
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo connected...'));
 
-app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
+// Routes
+app.get('/', (req, res) => res.send(`Store server running on port ${PORT}`));
+routes(app);
+
+// Starting server
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+});
